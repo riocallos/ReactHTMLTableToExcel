0a86741f85fd07f430e56ebad71b1337abb9455f
Rio Marie A. Callos <riomariecallos@gmail.com>
2020-08-18 13:09:31 +0800

    fixed button component

--- src/ReactHTMLTableToExcel.jsx
+++ src/ReactHTMLTableToExcel.jsx
@@ -102,15 +102,16 @@ class ReactHTMLTableToExcel extends Component {
   }
 
   render() {
+    const ButtonComponent = this.props.buttonComponent;
     return (
-      <RenderedButton
+      <ButtonComponent
           id={this.props.id}
           className={this.props.className}
           type="button"
           onClick={this.handleDownload}
       >
           {this.props.buttonText}
-      </RenderedButton>
+      </ButtonComponent>
     );
   }
 }
